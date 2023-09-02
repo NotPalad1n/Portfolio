3e1fa75b6c2e034012944a5569e2d44f3d848f89
unknown <younesoumast2@gmail.com>
2023-09-02 15:45:04 +0100

    added mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,31 +4,42 @@ import { HashLink } from "react-router-hash-link";
 import "../styles/Navbar.css"
 
 const Navbar = () => {
+  const [isClicked, setIsClicked] = React.useState(false)
+  
+  function toggle(){
+    setIsClicked(!isClicked)
+  }
+
   return (
-    <div className="navbar">
+    <div className={isClicked ? "navbar toggle" : "navbar"}>
       <div className="logo">
         <p className="icon">Y</p>
         <p>Younes Oumast</p>
       </div>
-      <ul>
+      <ul className={isClicked ? "nav-links active" : "nav-links"}>
         <li>
-          <HashLink to="#Home">Home</HashLink>
+          <HashLink to="#Home" onClick={toggle}>Home</HashLink>
         </li>
         <li>
-          <HashLink to="#About">About Me</HashLink>
+          <HashLink to="#About" onClick={toggle}>About Me</HashLink>
         </li>
         <li>
-          <HashLink to="#Services">Services</HashLink>
+          <HashLink to="#Services" onClick={toggle}>Services</HashLink>
         </li>
         <li>
-          <HashLink to="#Projects">Projects</HashLink>
+          <HashLink to="#Projects" onClick={toggle}>Projects</HashLink>
         </li>
         <li>
-          <HashLink to="#Contact" className="button">
+          <HashLink to="#Contact" className="button" onClick={toggle}>
             Contact Me
           </HashLink>
         </li>
       </ul>
+      <div className="hamburger-button" onClick={toggle}>
+        <div className="bar1"></div>
+        <div className="bar2"></div>
+        <div className="bar3"></div>
+      </div>
     </div>
   );
 }
